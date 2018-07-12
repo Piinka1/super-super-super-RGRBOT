79403d2a8c8f930eb2984fc2049df8aa9e0e129a
Piinka1 <41088681+Piinka1@users.noreply.github.com>
2018-07-12 06:48:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -202,26 +202,6 @@ client.on('ready',  () => {
   console.log(`Logged in as * [ " ${client.user.username} " ] Users! [ " ${client.users.size} " ]`);
   console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
 });
-client.on('message', message => {
-   let embed = new Discord.RichEmbed()
-
-    let args = message.content.split(' ').slice(1).join(' ');
-     if(!message.channel.guild) return;
-if(message.content.split(' ')[0] == '*bc') {
-         message.react("✔️")
-          let embed = new Discord.RichEmbed()
-    .setColor("#FF00FF")
-    .setThumbnail(message.author.avatarURL)   
-                                      .addField('تم الارسال بواسطة :', "<@" + message.author.id + ">")
-                 message.channel.sendEmbed(embed);
-        message.guild.members.forEach(m => {
-            var bc = new Discord.RichEmbed()             
-    .setColor('#ff0000')
-                 .addField('ّ', args, args)
-            m.send(``,{embed: bc});
-        });
-    }
-})
     client.on('message', message => {
      if (message.content === "*id") {
      let embed = new Discord.RichEmbed()
