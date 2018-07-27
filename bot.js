42ee64efbd80e3baddc472a4009c30f732a81369
Piinka1 <41088681+Piinka1@users.noreply.github.com>
2018-07-27 20:10:06 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,24 @@ client.on('ready', () => {
   console.log('Desert Bot Is Online')
   console.log('---------------')
 });
+  
+var AsciiTable = require('ascii-data-table').default
+client.on('message', message =>{
+
+    if(message.content == "+roles"){
+        var 
+        ros=message.guild.roles.size,
+        data = [['Rank', 'RoleName']]
+        for(let i =0;i<ros;i++){
+            if(message.guild.roles.array()[i].id !== message.guild.id){
+         data.push([i,`${message.guild.roles.filter(r => r.position == ros-i).map(r=>r.name)}`])
+        }}
+        let res = AsciiTable.table(data)
+
+        message.channel.send(`**\`\`\`xl\n${res}\`\`\`**`);
+    }
+});
+
 client.on('message', message => {
      if(message.content.startsWith(prefix + "hacker-2")) {
  let args = message.content.split(" ").slice(1);
